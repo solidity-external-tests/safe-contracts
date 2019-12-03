30bb24321c377ed3f2086bb93bfa89a541c896c6
Leonardo Alt <leo@ethereum.org>
2019-12-03 21:13:35 +0100

    Add virtual keyword to functions without implementation

--- contracts/mocks/Token.sol
+++ contracts/mocks/Token.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0 <0.7.0;
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
 abstract contract Token {
-	function transfer(address _to, uint value) public returns (bool);
+	function transfer(address _to, uint value) public virtual returns (bool);
 }
