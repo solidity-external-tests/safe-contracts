affcef8339a39d106d2e23e9dca8ee5f6306db64
chriseth <chris@ethereum.org>
2020-07-30 19:33:48 +0200

    Update version pragmas.

--- contracts/mocks/Token.sol
+++ contracts/mocks/Token.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.7.0;
+pragma solidity >=0.5.0 <0.8.0;
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
 abstract contract Token {
 	function transfer(address _to, uint value) public virtual returns (bool);
