f88447a09cc2de58037ad9a09e8b651b64370179
cameel <cameel2@gmail.com>
2020-12-01 00:00:51 +0100

    Rename inline assembly variables shadowing storage variables

--- contracts/proxies/GnosisSafeProxy.sol
+++ contracts/proxies/GnosisSafeProxy.sol
@@ -30,14 +30,14 @@ contract GnosisSafeProxy {
     {
         // solium-disable-next-line security/no-inline-assembly
         assembly {
-            let masterCopy := and(sload(0), 0xffffffffffffffffffffffffffffffffffffffff)
+            let masterCopy_ := and(sload(0), 0xffffffffffffffffffffffffffffffffffffffff)
             // 0xa619486e == keccak("masterCopy()"). The value is right padded to 32-bytes with 0s
             if eq(calldataload(0), 0xa619486e00000000000000000000000000000000000000000000000000000000) {
-                mstore(0, masterCopy)
+                mstore(0, masterCopy_)
                 return(0, 0x20)
             }
             calldatacopy(0, 0, calldatasize())
-            let success := delegatecall(gas(), masterCopy, 0, calldatasize(), 0, 0)
+            let success := delegatecall(gas(), masterCopy_, 0, calldatasize(), 0, 0)
             returndatacopy(0, 0, returndatasize())
             if eq(success, 0) { revert(0, returndatasize()) }
             return(0, returndatasize())
