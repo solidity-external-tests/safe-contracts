c99bebaa1c18b4f379383a7ddf5ed23965a3a063
cameel <cameel2@gmail.com>
2020-11-30 23:48:53 +0100

    Replace gas with gas()

--- contracts/common/SecuredTokenTransfer.sol
+++ contracts/common/SecuredTokenTransfer.sol
@@ -20,7 +20,7 @@ contract SecuredTokenTransfer {
         bytes memory data = abi.encodeWithSignature("transfer(address,uint256)", receiver, amount);
         // solium-disable-next-line security/no-inline-assembly
         assembly {
-            let success := call(sub(gas, 10000), token, 0, add(data, 0x20), mload(data), 0, 0)
+            let success := call(sub(gas(), 10000), token, 0, add(data, 0x20), mload(data), 0, 0)
             let ptr := mload(0x40)
             mstore(0x40, add(ptr, returndatasize()))
             returndatacopy(ptr, 0, returndatasize())
